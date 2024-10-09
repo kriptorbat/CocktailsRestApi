728d4517596ce6942e346b45b3c688a4ef71d364
kriptorbat <stasan.asanov@gmail.com>
2024-10-09 15:13:50 +0300

    init

---
+++ src/main/java/org/example/Controller.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class Controller {
+    private final String sharedKey = "SHARED_KEY";
+
+    private static final String SUCCESS_STATUS = "success";
+    private static final String ERROR_STATUS = "error";
+    private static final int CODE_SUCCESS = 100;
+    private static final int AUTH_FAILURE = 102;
+
+    @GetMapping
+    public String showPayment() {
+        return "Приветсутвую";
+    }
+}
